8d273ac87915b63de8502b4e555b8d02b5872611
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2023-01-15 01:34:19 +0200

    Added entity.

---
+++ src/main/java/com/example/costumers/entity/Customer.java
@@ -0,0 +1,55 @@
+package com.example.costumers.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String phoneNumber;
+    @Column(unique = true)
+    private String email;
+    private String app;
+    private LocalDate date;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getApp() {
+        return app;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
+
+
