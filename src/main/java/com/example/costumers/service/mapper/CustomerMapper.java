64c5e5c0c98f94a4633d6f48af937ae2b3ee9963
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2023-02-21 23:35:00 +0200

    Update

--- src/main/java/com/example/costumers/service/mapper/CustomerMapper.java
+++ src/main/java/com/example/costumers/service/mapper/CustomerMapper.java
@@ -3,12 +3,38 @@ package com.example.costumers.service.mapper;
 import com.example.costumers.dto.request.CustomerRequestDto;
 import com.example.costumers.dto.response.CustomerResponseDto;
 import com.example.costumers.entity.Customer;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
-public interface CustomerMapper {
-    Customer toEntity(CustomerRequestDto customerRequestDto);
+@Component
+public class CustomerMapper {
+    public Customer toEntity(CustomerRequestDto customerRequestDto) {
+        if (customerRequestDto == null) {
+            return null;
+        } else {
+            Customer customer = new Customer();
+            customer.setFirstName(customerRequestDto.getFirstName());
+            customer.setLastName(customerRequestDto.getLastName());
+            customer.setPhoneNumber(customerRequestDto.getPhoneNumber());
+            customer.setEmail(customerRequestDto.getEmail());
+            customer.setApp(customerRequestDto.getApp());
+            customer.setDate(customerRequestDto.getDate());
+            return customer;
+        }
+    }
 
-    CustomerResponseDto toDto(Customer customer);
+    public CustomerResponseDto toDto(Customer customer) {
+        if (customer == null) {
+            return null;
+        } else {
+            CustomerResponseDto customerResponseDto = new CustomerResponseDto();
+            customerResponseDto.setId(customer.getId());
+            customerResponseDto.setFirstName(customer.getFirstName());
+            customerResponseDto.setLastName(customer.getLastName());
+            customerResponseDto.setPhoneNumber(customer.getPhoneNumber());
+            customerResponseDto.setEmail(customer.getEmail());
+            customerResponseDto.setApp(customer.getApp());
+            customerResponseDto.setDate(customer.getDate());
+            return customerResponseDto;
+        }
+    }
 }
